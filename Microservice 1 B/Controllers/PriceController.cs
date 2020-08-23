614c6bb0b36100fc216c69076bf7de1b7c73b33b
vaskopozharski-00306661T <69101278+vaskopozharski-00306661T@users.noreply.github.com>
2020-08-23 23:01:53 +1000

    Revert "Revert "Microservice""
    This reverts commit 1362cf563ddae2b50f00c87b504b1fe0632895b8.

---
+++ Microservice 1 B/Controllers/PriceController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using static micrpService1.Startup;
+
+namespace micrpService1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PriceController : ControllerBase
+    {
+
+        private readonly ILogger<PriceController> _logger;
+
+        public PriceController(ILogger<PriceController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //public void loggers(string messagr)
+        //{
+
+        //    var url = "";
+        //    var client = new System.Net.WebClient();
+        //    //farkman
+        //    url = "http://18.224.96.81/log/log?i=" + messagr;
+        //    string temp = "";
+        //    string response = client.DownloadString(url);
+        //    if (!string.IsNullOrEmpty(response))
+        //    {
+
+        //        temp = response.ToString();
+               
+
+
+        //    }
+        //}
+        [HttpGet]
+        public string Get(string me)
+        {
+
+
+
+            LoggerSharedText.LogToServcie("request recived");
+
+
+
+            if (LoggerSharedText.Up == "ONLINE")
+            {
+                LoggerSharedText.LogToServcie("We are online and can serve requets");
+
+
+
+
+                if (me == null)
+                    me = "1";
+
+                if (me.Equals("1"))
+                {
+                    LoggerSharedText.LogToServcie("logg microservice 12 returned price $100");
+                    return "100 AUD";
+                }
+                if (me.Equals("2"))
+                {
+                    LoggerSharedText.LogToServcie("logg microservice 1  returned price $200");
+                    return "200 AUD";
+                }
+                if (me.Equals("3"))
+                {
+                    LoggerSharedText.LogToServcie("logg microservice 1  returned price $300");
+                    return "300 AUD";
+                }
+                else
+                {
+
+                    LoggerSharedText.LogToServcie("logg microservice 1 request price not found");
+                    return "price not found";
+                }
+
+            }
+            else
+            {
+                LoggerSharedText.LogToServcie("We are Offline and  and can not serve requets");
+                return "Microservice is Offline";
+
+            }
+        }
+    }
+}
---
+++ Microservice 1 B/Controllers/Up.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using static micrpService1.Startup;
+
+namespace micrpService1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UpController : ControllerBase
+    {
+
+        private readonly ILogger<UpController> _logger;
+
+        public UpController(ILogger<UpController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string me)
+        {
+            LoggerSharedText.LogToServcie("logg microservice 1 request recived for status");
+            LoggerSharedText.LogToServcie("logg microservice 1 returnt  status " + LoggerSharedText.Up);
+
+            return LoggerSharedText.Up;
+
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StopController : ControllerBase
+    {
+
+        private readonly ILogger<UpController> _logger;
+
+      
+
+        [HttpGet]
+        public void Get(string me)
+        {
+            LoggerSharedText.LogToServcie("logg microservice 1 request recived to STOP service");
+
+             LoggerSharedText.Up = "STOPPED";
+
+
+            LoggerSharedText.LogToServcie("logg microservice 1  service is stopped " + LoggerSharedText.Up);
+
+
+
+
+        }
+    }
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StartController : ControllerBase
+    {
+
+        private readonly ILogger<StartController> _logger;
+
+
+
+        [HttpGet]
+        public void Get(string me)
+        {
+            LoggerSharedText.LogToServcie("logg microservice 1 request recived to START service");
+
+            LoggerSharedText.Up = "ONLINE";
+
+
+            LoggerSharedText.LogToServcie("logg microservice 1  service is : " + LoggerSharedText.Up);
+
+
+
+
+        }
+    }
+
+
+
+}
